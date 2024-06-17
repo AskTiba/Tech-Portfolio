f4731be3776a920feb8d3b242a79e59839a03c57
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-06-17 23:04:49 +0300

    Setup Shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+
 export default function Home() {
   const router = useRouter();
   const handleClick = () => {
@@ -11,13 +13,13 @@ export default function Home() {
       <div className="uppercase flex justify-center items-center text-5xl font-bold tracking-wider">
         welcome to my project hub
       </div>
-      <button
+      <Button
         type="button"
         onClick={handleClick}
         className="bg-orange text-blue font-bold my-6 py-3 px-5 rounded-3xl text-xl transition-all duration-200 hover:bg-white"
       >
         Interactive Rating Component
-      </button>
+      </Button>
     </main>
   );
 }
