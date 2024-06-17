d3ec0f8bb53ac5913e3bd32066d238f7dffa30af
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-06-17 20:48:37 +0300

    Introduction

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main className="flex text-[15px] text-white bg-dark-blue h-screen w-screen justify-center items-center">
+      <div className="uppercase flex justify-center items-center text-5xl font-bold tracking-wider">
+        welcome to my project hub
+      </div>
+    </main>
+  );
+}
